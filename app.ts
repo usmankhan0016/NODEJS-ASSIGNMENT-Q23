7914a8a50fca1bd7cd544290bcd947f2e9a9e82b
Usman Khan <160907897+usmankhan0016@users.noreply.github.com>
2024-04-07 14:02:18 -0700

    First commit

---
+++ app.ts
@@ -0,0 +1,68 @@
+// Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test.
+
+// Creating a variable:
+let car: string = "subaru";
+
+// Test no:1
+
+console.log("Is car == 'subaru'? I predict True.")
+
+console.log(car == 'subaru');
+
+// Test no:2
+
+console.log("Is car === 'subaru'? I predict True.")
+
+console.log(car === 'subaru');
+
+// Test no:3
+
+console.log("Is car != 'Honda'? I predict True.")
+
+console.log(car != 'Honda');
+
+// Test no:4
+
+console.log("Is car !== 'Honda'? I predict True.")
+
+console.log(car !== 'Honda');
+
+// Test no:5
+
+console.log("Is car.Upper Case == 'SUBARU'? I predict True.")
+
+console.log(car.toUpperCase() == 'SUBARU');
+
+// Test no:6
+
+console.log("Is car == 'SUBARU'? I predict False.")
+
+console.log(car == 'SUBARU');
+
+// Test no:7
+
+console.log("Is car === 'SUBARU'? I predict False.")
+
+console.log(car === 'SUBARU');
+
+// Test no:8
+
+console.log("Is car === 'Subaru'? I predict False.")
+
+console.log(car === 'Subaru');
+
+// Test no:9
+
+console.log("Is car =='bike'? I predict False.")
+
+console.log(car == 'bike');
+
+// Test no:10
+
+console.log("Is car == 'cycle'? I predict False.")
+
+console.log(car == 'cycle');
+
+
+
+
